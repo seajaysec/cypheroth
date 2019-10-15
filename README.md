2d2190537c062476e91864226cf37dc7df7bb04f
Chris Johnson <cfjohnson@gmail.com>
2019-10-15 16:16:36 -0400

    Initial Upload

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # cypheroth
  Automated, extensible toolset that runs cypher queries against Bloodhound's neo4j backend and saves output to csv.
+
+
+<p align="center">
+  <img src="./img/cypheroth.png" alt="cypheroth"/>
+</p>
+
+
+### Documentation
+
+This is a bash script that automates running cypher queries against Bloodhound data stored in a neo4j database.
+
+I found myself re-running the same queries through the neo4j web interface on multiple assessments and figured there must be an easier way. 😅
+
+The cypher query is fully extensible. See below a formatting example.
+
+Please share any additional useful queries so I can add them to this project!
+
+### Cypher Queries
+
+The current query set requests the following information:
+
+* Full User Property List
+* Full Computer Property List
+* Full Domain Property List
+* Full OU Property List
+* Full GPO Property List
+* Full Group Property List
+* Computers with Admins
+* Computers without Admins
+* Groups with Computers and Admins
+* Group Admin Info
+* Users that are not AdminCount 1, have generic all, and no local admin
+* Users that are admin on 1+ machines, sorted by admin count
+* Kerberoastable users sorted by total machine admin count
+* Kerberoastable users and computers where they are admins
+* Computers that members of the Domain Users group can RDP to
+* Computers where users which can Return, if they belong to adm or svr accounts
+* Computer names where each domain user has derivative Admin privileges to
+* Users with paths to High Value groups
+* Every computer account that has local admin rights on other computers
+* Find which domain Groups are Admins to what computers
+* What permissions does Everyone/Authenticated users/Domain users/Domain computers have
+* All users with SPN in Domain Admin group, with enabled status and unconstrained delegation status displayed
+
+To add additional queries, edit `queries.txt` and add a line using the following format:
+
+`Description;Cypher Query;Output File`
+
+Example: `All Usernames;MATCH (u:User) RETURN u.name;usernames.csv`
+
+### Author
+Chris Farrell ([@seajay](https://twitter.com/seajay))
+
+### Acknowledgments
+
+* This tool wouldn't exist without BloodHound - developed by [@_wald0](https://twitter.com/_wald0), [@CptJesus](https://twitter.com/CptJesus), and [@harmj0y](https://twitter.com/harmj0y).
+* Shoutout to the [Bloodhound Slack](https://bloodhoundgang.herokuapp.com) `#cypher_queries` channel for assistance
+* Big ups to [@TinkerSec](https://twitter.com/TinkerSec) - the bones of this project were straight up copy/pasted from his [procdump script](https://github.com/tinkersec/scratchpad/blob/master/BashScripts/grabDump.sh) 🙃
+* Many thanks to [@awsmhacks](https://twitter.com/awsmhacks) and [@haus3c](https://twitter.com/haus3c) for collecting useful cypher queries ([here](https://github.com/awsmhacks/awsmBloodhoundCustomQueries) and [here](https://hausec.com/2019/09/09/bloodhound-cypher-cheatsheet/))
---
+++ cypheroth.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env bash
+
+# Check if required tool cypher-shell is installed
+if which cypher-shell >/dev/null; then
+    echo -e "\e[1mcypher-shell installation verified\e[22m\n\n"
+else
+    echo -e "\e[1mInstall cypher-shell\e[22m"
+    exit 1
+fi
+
+USAGE="
+                    __                    __  __  
+  _______  ______  / /_  ___  _________  / /_/ /_ 
+ / ___/ / / / __ \\/ __ \/ _ \/ ___/ __ \\/ __/ __ \\
+/ /__/ /_/ / /_/ / / / /  __/ /  / /_/ / /_/ / / /
+\___/\__, / .___/_/ /_/\___/_/   \____/\__/_/ /_/ 
+    /____/_/                                      
+
+Flags:
+  -u	Neo4J Username (Required)
+  -p	Neo4J Password (Required)
+  -d	Fully Qualified Domain Name (Required)
+  -v    Verbose mode (Optional) (Default:FALSE)
+  -h	Help text and usage example (Optional)
+
+Example: ./cypheroth.sh -u neo4j -p neo4jj -d testlab.local -v true
+
+Files are added to the ./cypherout directory
+"
+
+# Check if any flags were set. If not, print out help.
+if [ $# -eq 0 ]; then
+    echo "$USAGE"
+    exit
+fi
+
+# Initial variable state
+VERBOSE='FALSE'
+
+# Flag configuration
+while getopts "u:p:d:v:h" FLAG; do
+    case $FLAG in
+    u)
+        USERNAME=$OPTARG
+        ;;
+    p)
+        PASSWORD=$OPTARG
+        ;;
+    d)
+        DOMAIN=$OPTARG
+        ;;
+    v)
+        VERBOSE=$OPTARG
+        ;;
+    h)
+        echo "$USAGE"
+        exit
+        ;;
+    *)
+        echo "$USAGE"
+        exit
+        ;;
+    esac
+done
+
+# Make sure each required flag was actually set.
+if [ -z ${USERNAME+x} ]; then
+    echo "Username flag (-u) is not set."
+    echo "$USAGE"
+    exit
+elif [ -z ${PASSWORD+x} ]; then
+    echo "Password flag (-p) is not set."
+    echo "$USAGE"
+    exit
+elif [ -z ${DOMAIN+x} ]; then
+    echo "User domain flag (-f) is not set."
+    echo "$USAGE"
+    exit
+fi
+
+# Make sure $DOMAIN is UpperCase
+DOMAIN=${DOMAIN^^}
+
+# Make sure $VERBOSE is UpperCase
+VERBOSE=${VERBOSE^^}
+
+# Create output dir
+mkdir ./cypherout 2>/dev/null
+
+# Sanitize cypher queries
+sed -i '/^$/d' ./queries.txt
+echo "" >>./queries.txt
+
+# Set aliases
+n4jV="cypher-shell -u $USERNAME -p $PASSWORD --format verbose"
+n4jP="cypher-shell -u $USERNAME -p $PASSWORD --format plain"
+
+# The meat and potatoes
+cat queries.txt | while read line; do
+    DESCRIPTION=$(echo $line | cut -d ';' -f 1)
+    QUERY=$(echo $line | cut -d ';' -f 2)
+    OUTPUT=$(echo $line | cut -d ';' -f 3)
+    echo -e "\e[1m$DESCRIPTION\e[22m"
+    if [ "$VERBOSE" == "TRUE" ]; then
+        tput rmam
+        $n4jV "$QUERY" | sed \$d
+        tput smam
+    fi
+    $n4jP "$QUERY" >./cypherout/$OUTPUT
+    echo -e "\e[3mSaved to ./cypherout/$OUTPUT\e[23m\n"
+done
+
+# Delete Empty Output Files
+find ./cypherout/ -type f -size 1 | xargs rm -f
+
+# If ssconvert is installed, join all .csv output to .xls
+if which ssconvert >/dev/null; then
+    ssconvert --merge-to=./cypherout/all.xls ./cypherout/*.csv
+    echo -e "\e[1mAll CSVs joined to ./cypherout/all.xls\e[22m"
+    echo
+else
+    echo -e "\e[1mInstall ssconvert (apt or brew install gnumeric) to auto-join csv output to sheets in an xls workbook.\e[22m"
+fi
